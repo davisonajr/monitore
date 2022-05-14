dffc5c40caa1b8d9d7fbfff8798feeeb7ac58746
Davison Almeida <davison.almeida@leolearning.com>
2022-05-14 15:36:35 -0300

    config heroku

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	router := gin.Default()
 	router.TrustedPlatform = gin.PlatformGoogleAppEngine
 	router.LoadHTMLGlob("pages/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	router.Run(":9000")
+	router.Run(":" + port)
 }
