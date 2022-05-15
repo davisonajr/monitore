384514437439c0298c002f8f5631f8e8a09ec2de
Davison Almeida <davison.almeida@leolearning.com>
2022-05-14 23:01:06 -0300

    incorpora arquivos estáticos

--- main.go
+++ main.go
@@ -1,26 +1,59 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	port := os.Getenv("PORT")
-
+	//port := os.Getenv("PORT")
+	port := "8080"
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
 	router := gin.Default()
+
+	router.Static("/assets", "./assets")
+
 	router.TrustedPlatform = gin.PlatformGoogleAppEngine
+
 	router.LoadHTMLGlob("pages/*")
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	router.Run(":" + port)
+
+	srv := &http.Server{
+		Addr:    (":" + port),
+		Handler: router,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
